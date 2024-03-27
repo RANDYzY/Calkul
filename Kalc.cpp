df464ae9415ab207ead575e217160496d2021c56
Zelkowsky <dmitrymuhametov385@yandex.ru>
2024-03-27 16:14:18 +0300

    first commit

---
+++ Kalc.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    char op;
+    double num1, num2;
+
+    cout << "Введите первое число: ";
+    cin >> num1;
+
+    cout << "Выберите операцию (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    switch (op) {
+    case '+':
+        cout << "Результат: " << num1 + num2;
+        break;
+    case '-':
+        cout << "Результат: " << num1 - num2;
+        break;
+    case '*':
+        cout << "Результат: " << num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Результат: " << num1 / num2;
+        }
+        else {
+            cout << "Ошибка: на ноль не делиться!";
+        }
+        break;
+    default:
+        cout << "Ошибка: неправильная операция!";
+    }
+
+    return 0;
+}
